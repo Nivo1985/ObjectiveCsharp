f1794153c1564119a607034021b4b825f3f15b99
Karol Rogowski <karol.rogowski@gmail.com>
2016-11-26 19:09:47 +0100

    immutable objects

---
+++ 3)Immutable Objects/Immutable Objects/MoneyAmount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immutable_Objects
+{
+    sealed class MoneyAmount: IEquatable<MoneyAmount>
+    {
+        public decimal Amount { get;}
+        public string CurrencySymbol { get;}
+
+        public MoneyAmount(decimal amount, string currencySymbol)
+        {
+            Amount = amount;
+            CurrencySymbol = currencySymbol;
+        }
+        
+        public override string ToString() => $"{this.Amount} {this.CurrencySymbol}";
+
+        public static MoneyAmount operator *(MoneyAmount amount, decimal factory) => amount.Scale(factory);
+
+        public static bool operator ==(MoneyAmount a, MoneyAmount b) =>
+            (object.ReferenceEquals(a, null) && object.ReferenceEquals(b, null)) ||
+            (!object.ReferenceEquals(a, null) && a.Equals(b));
+
+        public static bool operator !=(MoneyAmount a, MoneyAmount b) => !(a == b);
+
+        public MoneyAmount Scale(decimal factor) =>
+            new MoneyAmount(this.Amount * factor, this.CurrencySymbol);
+
+        public override int GetHashCode() =>
+            this.Amount.GetHashCode() ^ this.CurrencySymbol.GetHashCode();
+
+        public override bool Equals(object obj) =>
+            this.Equals(obj as MoneyAmount);
+
+        public bool Equals(MoneyAmount other) =>
+            other != null &&
+            this.Amount == other.Amount &&
+            this.CurrencySymbol == other.CurrencySymbol;
+    }
+}
---
+++ 3)Immutable Objects/Immutable Objects/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immutable_Objects
+{
+    class Program
+    {
+        static bool IsHappyHour { get; set; }
+
+
+
+        static MoneyAmount Reserve(MoneyAmount cost)
+        {
+            decimal factory = 1;
+
+            if (IsHappyHour)
+            {
+                factory = .5M;
+            }
+
+            Console.WriteLine("Reserved cost {0}", cost);
+            return cost * factory;
+        }
+
+        static void Buy(MoneyAmount wallet, MoneyAmount cost)
+        {
+            var enoughtMoney = wallet.Amount >= cost.Amount;
+
+            var finalCost = Reserve(cost);
+
+            var finalEnough = wallet.Amount >= finalCost.Amount;
+
+            if (enoughtMoney && finalEnough)
+                Console.WriteLine("You will pay {0} with your {1}", cost, wallet);
+            else if (finalEnough)
+            {
+                Console.WriteLine("This time, {0} will be enough to pay {1}", wallet, finalEnough);
+            }
+            else
+                Console.WriteLine("You cannot pay {0} with your {1}", cost, wallet);
+        }
+
+        static void Main(string[] args)
+        {
+            #region Buying Flow
+            //Buy(new MoneyAmount(12, "USD"),
+            //    new MoneyAmount(10, "USD"));
+
+            //Buy(new MoneyAmount(7, "USD"),
+            //    new MoneyAmount(10, "USD"));
+
+            //IsHappyHour = true;
+            //Buy(new MoneyAmount(7, "USD"),
+            //    new MoneyAmount(10, "USD")); 
+            #endregion
+
+            #region Equels
+            //var x = new MoneyAmount(2, "USD");
+            //var y = new MoneyAmount(4, "USD");
+
+            //if (x.Equals(y))
+            //{
+            //    Console.WriteLine("x == y");
+            //}
+            //if ((x * 2).Equals(y))
+            //{
+            //    Console.WriteLine("x * 2 == y"); 
+            //}
+            #endregion
+
+            #region Hash
+
+            var x = new MoneyAmount(2,"USD");
+            var y = new MoneyAmount(2, "USD");
+
+            HashSet<MoneyAmount> set = new HashSet<MoneyAmount>();
+
+            set.Add(x);
+
+            if (set.Contains(y))
+                Console.WriteLine("cannot add value");
+            else
+                set.Add((y));
+
+            Console.WriteLine("Count = {0}", set.Count);
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
+
